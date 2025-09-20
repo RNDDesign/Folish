2c84c0ccca52b29676890195aa7337f06e560d33
SubhaNITS150 <subhajyotidey2910@gmail.com>
2025-09-20 19:29:24 +0530

    Added folders

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:folish/app.dart';
+import 'package:folish/utils/theme/theme.dart';
+
+void main() {
+
+  // 1. Add widets Binding
+  // 2. Init local storage
+  // 3. Await Native Splash
+  // 4. Initialise Firebase
+  // 5. Initialise Authentication
+
+  runApp(const App());
+}
+
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,16 @@
+class TTexts {
+  // Global texts
+
+  // Onboarding texts
+  static const String onboardingTitle1 = "";
+  static const String onboardingTitle2 = "";
+  static const String onboardingTitle3 = "";
+
+  static const String onboardingSubTitle1 = "";
+  static const String onboardingSubTitle2 = "";
+  static const String onboardingSubTitle3 = "";
+
+  // Home texts
+  static const String homeAppBarTitle = "";
+  static const String homeAppBarSubTitle = "";
+}
